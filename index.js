50e0e233b53a95c730e698d6c3637c8655bff0cb
aorus22 <alyzanadif04@gmail.com>
2024-06-24 20:28:17 +0700

    ubah tulisan running port

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.use('/', indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
